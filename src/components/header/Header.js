8f7d1282e89f423531acea77ba7a4301005f278c
callmeteddy17 <dtd172000@gmail.com>
2023-03-05 02:59:13 +0700

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import './header.css';
-import logo from 'https://github.com/callmeteddy17/project/blob/main/src/assets/images/Logo.png';
+import logo from '../../assets/images/Logo.png';
+import '../header/header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 
 function Header() {
     return (
-        <header>
-            <a href="siteofteddy.click">
-                <img src={logo} alt={'logo'} />
-            </a>
+        <header className="header">
+            <div className="icon" href="">
+                <img id="logo" src={logo} alt={'logo'} />
+                <span>TEDDY DO</span>
+            </div>
+            <p className="icon">
+                <FontAwesomeIcon icon={faBars} style={{ height: 40 }}></FontAwesomeIcon>
+            </p>
         </header>
     );
 }
